7f150fcb5518d5eec04fa3c7174e24995516ae3d
Ilya <ilia78-03@mail.ru>
2021-12-18 03:00:11 +0300

    front pages

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuscSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+        public IActionResult RegPage()
+        {
+            return View();
+        }
+        public IActionResult NewsPage()
+        {
+            return View();
+        }
+    }
+}
